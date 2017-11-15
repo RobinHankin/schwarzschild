f4fcfb165a77e5b769692714c1b09865d56bb109
RobinHankin <hankin.robin@gmail.com>
2017-11-15 20:29:47 +1300

    changes to include the animation stuff

---
+++ R/GW_animation.R
@@ -0,0 +1,42 @@
+GW <- function(plus=0.03, cross=0, lattice=TRUE, frames=20, n=11, center=c(0,0)){
+    
+    jj <- seq(from= -1,to= 1,len=n)
+
+    if(lattice){
+        p <- as.matrix(expand.grid(jj,jj))
+    } else {
+        jj <- jj*(1-1/n)*pi
+        p <- cbind(sin(jj),cos(jj))*0.9
+    }
+    
+    par(pty="m") # forces a square plot
+    ani.record(reset = TRUE)  # clear history before recording
+    
+    for(time in seq(from=0,to=2*pi*(1-1/frames),len=frames)){
+        
+        plus_perturb <-
+            plus*cbind(
+                     +exp(1i*time)*(p[,1]-center[1]),
+                     -exp(1i*time)*(p[,2]-center[2])
+                 )
+        
+        cross_perturb <- 
+            cross*cbind(
+                      +exp(1i*time)*(p[,2]-center[2]),
+                      +exp(1i*time)*(p[,1]-center[1])
+                  )
+                
+    { # plot commands start...
+        plot(NULL,xlim=c(-1,1),ylim=c(-1,1),xlab='',ylab='',asp=1,axes=FALSE,type='n')
+#        axis(1,pos=-1.1)
+#        axis(2,pos=-1.1)
+        points(p+Re(plus_perturb + cross_perturb), type='p')
+    } #  plot commands end
+        
+        ani.record()  # record the current frame
+    }  # time loop closes
+    
+    oopts <- ani.options(interval = 0.05)
+    while(TRUE){ani.replay()}
+    
+} # function definition closes
