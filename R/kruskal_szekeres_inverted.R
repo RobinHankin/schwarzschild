d3606092ea5f6aaab8e9ad331141b8a8d2919ce4
RobinHankin <hankin.robin@gmail.com>
2018-03-05 20:51:10 +1300

    proper indentation

--- R/kruskal_szekeres_inverted.R
+++ R/kruskal_szekeres_inverted.R
@@ -1,58 +1,57 @@
 kruskal_inverted <- function(colours=standard_colours, ...){
 
-
-  n <- 4 # size of plot
-  par(xpd=FALSE)
-  clip(0,n,0,n)
-plot(NULL,xlim=c(0,n),ylim=c(0,n),asp=1,type="n",axes=FALSE,
-     xlab='Schwarzschild r',ylab='Schwarzschild t',main="Inverted Kruskal-Szekeres coordinates")
-
-axis(1,pos=0,at=0:n)
-axis(2,pos=0,at=0:n)
-
-  for(i in 0:4){ segments(x0=0,x1=n+1,y0=i,col=colours$t)}
-  for(i in 0:4){ segments(x0=i,y0=0,y1=n+1,col=colours$r)}
-
- ## Tval and Xval are Kruskal X,T.
-
-Tval <- seq(from=-33,to=33,len=10000)
-Xval <- c(0,0.5,1,2,4,8,16,32)
-
-
-## first, lines of constant X
-for(i in Xval){
-  points(RT(cbind(i,Tval)),type='l',col=colours$kruskal_X)
-}
-
-abline(v=0)
-abline(v=1)
-
-
-Tval <- c(0,0.5,1,2,4,8,16)
-Xval <- seq(from=-33,to=33,len=10000)
-
-
-## second, lines of constant T
-for(i in Tval){
-  points(RT(cbind(Xval,i)),type='l',col=colours$kruskal_T)
-}
-
-  legend(x=2.1, y=3.9, lty=1, bg="white",
-         legend=c(
-             "lines of constant Schwarzschild r",
-             "lines of constant Schwarzschild t",
-             "lines of constant X",
-             "lines of constant T"
-             ),
-         col=c(
-             colours$r,
-             colours$t,
-             colours$kruskal_X,
-             colours$kruskal_T
-         )
-  )
-
+    n <- 4 # size of plot
+    par(xpd=FALSE)
+    clip(0,n,0,n)
+    plot(NULL,xlim=c(0,n),ylim=c(0,n),asp=1,type="n",axes=FALSE,
+         xlab='Schwarzschild r',ylab='Schwarzschild t',main="Inverted Kruskal-Szekeres coordinates")
+    
+    axis(1,pos=0,at=0:n)
+    axis(2,pos=0,at=0:n)
+    
+    for(i in 0:4){ segments(x0=0,x1=n+1,y0=i,col=colours$t)}
+    for(i in 0:4){ segments(x0=i,y0=0,y1=n+1,col=colours$r)}
+    
+    ## Tval and Xval are Kruskal X,T.
+    
+    Tval <- seq(from=-33,to=33,len=10000)
+    Xval <- c(0,0.5,1,2,4,8,16,32)
+    
+    
+    ## first, lines of constant X
+    for(i in Xval){
+        points(RT(cbind(i,Tval)),type='l',col=colours$kruskal_X)
+    }
+    
+    abline(v=0)
+    abline(v=1)
+    
+    
+    Tval <- c(0,0.5,1,2,4,8,16)
+    Xval <- seq(from=-33,to=33,len=10000)
+    
+    
+    ## second, lines of constant T
+    for(i in Tval){
+        points(RT(cbind(Xval,i)),type='l',col=colours$kruskal_T)
+    }
+    
+    legend(x=2.1, y=3.9, lty=1, bg="white",
+           legend=c(
+               "lines of constant Schwarzschild r",
+               "lines of constant Schwarzschild t",
+               "lines of constant X",
+               "lines of constant T"
+           ),
+           col=c(
+               colours$r,
+               colours$t,
+               colours$kruskal_X,
+               colours$kruskal_T
+           )
+           )
+    
     abline(v=0,lwd=5)
-  abline(v=1,lwd=5,col=colours$horizon)
-
+    abline(v=1,lwd=5,col=colours$horizon)
+    
 }
