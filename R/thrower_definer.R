4c70bda29387b4fffad1d81945f445d4c668ea5b
RobinHankin <hankin.robin@gmail.com>
2018-03-07 21:04:51 +1300

    proper indentation

--- R/thrower_definer.R
+++ R/thrower_definer.R
@@ -7,43 +7,43 @@
          type='n',xlim=c(0.1,10),ylim=c(0.5,50),
          log=logaxes, xlab='Schwarzschild r',ylab='Schwarzschild t',
          main = "Black hole in Schwarzschild coordinates, with worldlines of free particles", ...)
-
-r_start_outside <- 2
-r_start_inside <- 0.9
-
-t_start_outside <- 0.8
-t_start_inside <- 2.0
-
-lthick <- 2
     
-points(trajectory(t0=t_start_outside, r0=r_start_outside, 0.82,sign=-1),type='l',lwd=lthick,col='red')
-points(trajectory(t0=t_start_outside, r0=r_start_outside, 0.82,sign=1),type='l',lwd=lthick,col='orange')
-points(trajectory(t0=t_start_outside, r0=r_start_outside, 0.89,sign=1,n=1000),type='l',lwd=lthick,col='orange')
-points(trajectory(t0=t_start_outside, r0=r_start_outside, 0.88,sign=1,n=1000),type='l',lwd=lthick,col='orange')
-points(trajectory(t0=t_start_outside, r0=r_start_outside, 0.87,sign=1,n=1000),type='l',lwd=lthick,col='orange')
-points(trajectory(t0=t_start_outside, r0=r_start_outside, 0.91,sign=1,n=1000),type='l',lwd=lthick,col='orange')
-points(trajectory(t0=t_start_outside, r0=r_start_outside, 1.00,sign=1,n=1000),type='l',lwd=lthick,col='yellow')
-points(trajectory(t0=t_start_outside, r0=r_start_outside, 1.89,sign=1),type='l',lwd=lthick,col='green')
-points(trajectory(t0=t_start_outside, r0=r_start_outside, 189,sign=1),type='l',lwd=lthick,col='green')
+    r_start_outside <- 2
+    r_start_inside <- 0.9
+    
+    t_start_outside <- 0.8
+    t_start_inside <- 2.0
 
-points(trajectory(t0=t_start_inside,r0=r_start_inside,0.3,sign= +1,n=1000),type='l',lwd=lthick,col='blue')
-points(trajectory(t0=t_start_inside,r0=r_start_inside,0.91,sign= +1,n=1000),type='l',lwd=lthick,col='blue')
+    lthick <- 2
+    
+    points(trajectory(t0=t_start_outside, r0=r_start_outside, 0.82,sign=-1),type='l',lwd=lthick,col='red')
+    points(trajectory(t0=t_start_outside, r0=r_start_outside, 0.82,sign=1),type='l',lwd=lthick,col='orange')
+    points(trajectory(t0=t_start_outside, r0=r_start_outside, 0.89,sign=1,n=1000),type='l',lwd=lthick,col='orange')
+    points(trajectory(t0=t_start_outside, r0=r_start_outside, 0.88,sign=1,n=1000),type='l',lwd=lthick,col='orange')
+    points(trajectory(t0=t_start_outside, r0=r_start_outside, 0.87,sign=1,n=1000),type='l',lwd=lthick,col='orange')
+    points(trajectory(t0=t_start_outside, r0=r_start_outside, 0.91,sign=1,n=1000),type='l',lwd=lthick,col='orange')
+    points(trajectory(t0=t_start_outside, r0=r_start_outside, 1.00,sign=1,n=1000),type='l',lwd=lthick,col='yellow')
+    points(trajectory(t0=t_start_outside, r0=r_start_outside, 1.89,sign=1),type='l',lwd=lthick,col='green')
+    points(trajectory(t0=t_start_outside, r0=r_start_outside, 189,sign=1),type='l',lwd=lthick,col='green')
 
-points(trajectory(t0=t_start_inside,r0=r_start_inside,0.3,sign= -1,n=1000),type='l',lwd=lthick,col='purple')
-points(trajectory(t0=t_start_inside,r0=r_start_inside,0.91,sign= -1,n=1000),type='l',lwd=lthick,col='purple')
+    points(trajectory(t0=t_start_inside,r0=r_start_inside,0.3,sign= +1,n=1000),type='l',lwd=lthick,col='blue')
+    points(trajectory(t0=t_start_inside,r0=r_start_inside,0.91,sign= +1,n=1000),type='l',lwd=lthick,col='blue')
+
+    points(trajectory(t0=t_start_inside,r0=r_start_inside,0.3,sign= -1,n=1000),type='l',lwd=lthick,col='purple')
+    points(trajectory(t0=t_start_inside,r0=r_start_inside,0.91,sign= -1,n=1000),type='l',lwd=lthick,col='purple')
 
     
-## event horizon:
-abline(v=1,lwd=6,col=colours$horizon)
+    ## event horizon:
+    abline(v=1,lwd=6,col=colours$horizon)
 
-## source of throwing:
-points(r_start_outside,t_start_outside,pch=16)
-points(r_start_inside,t_start_inside,pch=16)
+    ## source of throwing:
+    points(r_start_outside,t_start_outside,pch=16)
+    points(r_start_inside,t_start_inside,pch=16)
 
-legend(legpos,lty=1,lwd=c(rep(lthick,6),6),
-       col=c("red","orange","yellow","green","blue","purple",colours$horizon),
-       legend=c("inward throw","outward throw, bounded","critical","unbounded",
-                "interior inward","interior outward","event horizon"))
+    legend(legpos,lty=1,lwd=c(rep(lthick,6),6),
+           col=c("red","orange","yellow","green","blue","purple",colours$horizon),
+           legend=c("inward throw","outward throw, bounded","critical","unbounded",
+                    "interior inward","interior outward","event horizon"))
 }
 
 `thrower_asp1`  <- function(colours=standard_colours, ...){
@@ -55,39 +55,39 @@ legend(legpos,lty=1,lwd=c(rep(lthick,6),6),
     axis(1,pos=0,at=c(0,5,10))
     axis(2,pos=0)
 
-r_start_outside <- 2
-r_start_inside <- 0.9
+    r_start_outside <- 2
+    r_start_inside <- 0.9
 
-t_start_outside <- 0.8
-t_start_inside <- 2.0
+    t_start_outside <- 0.8
+    t_start_inside <- 2.0
 
-lthick <- 2
+    lthick <- 2
     
-points(trajectory(t0=t_start_outside, r0=r_start_outside, 0.82,sign=-1),type='l',lwd=lthick,col='red')
-points(trajectory(t0=t_start_outside, r0=r_start_outside, 0.82,sign=1),type='l',lwd=lthick,col='orange')
-points(trajectory(t0=t_start_outside, r0=r_start_outside, 0.89,sign=1,n=1000),type='l',lwd=lthick,col='orange')
-points(trajectory(t0=t_start_outside, r0=r_start_outside, 0.88,sign=1,n=1000),type='l',lwd=lthick,col='orange')
-points(trajectory(t0=t_start_outside, r0=r_start_outside, 0.87,sign=1,n=1000),type='l',lwd=lthick,col='orange')
-points(trajectory(t0=t_start_outside, r0=r_start_outside, 0.91,sign=1,n=1000),type='l',lwd=lthick,col='orange')
-points(trajectory(t0=t_start_outside, r0=r_start_outside, 1.00,sign=1,n=1000),type='l',lwd=lthick,col='yellow')
-points(trajectory(t0=t_start_outside, r0=r_start_outside, 1.89,sign=1),type='l',lwd=lthick,col='green')
-points(trajectory(t0=t_start_outside, r0=r_start_outside, 189,sign=1),type='l',lwd=lthick,col='green')
+    points(trajectory(t0=t_start_outside, r0=r_start_outside, 0.82,sign=-1),type='l',lwd=lthick,col='red')
+    points(trajectory(t0=t_start_outside, r0=r_start_outside, 0.82,sign=1),type='l',lwd=lthick,col='orange')
+    points(trajectory(t0=t_start_outside, r0=r_start_outside, 0.89,sign=1,n=1000),type='l',lwd=lthick,col='orange')
+    points(trajectory(t0=t_start_outside, r0=r_start_outside, 0.88,sign=1,n=1000),type='l',lwd=lthick,col='orange')
+    points(trajectory(t0=t_start_outside, r0=r_start_outside, 0.87,sign=1,n=1000),type='l',lwd=lthick,col='orange')
+    points(trajectory(t0=t_start_outside, r0=r_start_outside, 0.91,sign=1,n=1000),type='l',lwd=lthick,col='orange')
+    points(trajectory(t0=t_start_outside, r0=r_start_outside, 1.00,sign=1,n=1000),type='l',lwd=lthick,col='yellow')
+    points(trajectory(t0=t_start_outside, r0=r_start_outside, 1.89,sign=1),type='l',lwd=lthick,col='green')
+    points(trajectory(t0=t_start_outside, r0=r_start_outside, 189,sign=1),type='l',lwd=lthick,col='green')
 
-points(trajectory(t0=t_start_inside,r0=r_start_inside,0.3,sign= +1,n=1000),type='l',lwd=lthick,col='blue')
-points(trajectory(t0=t_start_inside,r0=r_start_inside,0.91,sign= +1,n=1000),type='l',lwd=lthick,col='blue')
+    points(trajectory(t0=t_start_inside,r0=r_start_inside,0.3,sign= +1,n=1000),type='l',lwd=lthick,col='blue')
+    points(trajectory(t0=t_start_inside,r0=r_start_inside,0.91,sign= +1,n=1000),type='l',lwd=lthick,col='blue')
 
-points(trajectory(t0=t_start_inside,r0=r_start_inside,0.3,sign= -1,n=1000),type='l',lwd=lthick,col='purple')
-points(trajectory(t0=t_start_inside,r0=r_start_inside,0.91,sign= -1,n=1000),type='l',lwd=lthick,col='purple')
+    points(trajectory(t0=t_start_inside,r0=r_start_inside,0.3,sign= -1,n=1000),type='l',lwd=lthick,col='purple')
+    points(trajectory(t0=t_start_inside,r0=r_start_inside,0.91,sign= -1,n=1000),type='l',lwd=lthick,col='purple')
 
-## event horizon:
-abline(v=1,lwd=6,col=colours$horizon)
+    ## event horizon:
+    abline(v=1,lwd=6,col=colours$horizon)
 
-## source of throwing:
-points(r_start_outside,t_start_outside,pch=16)
-points(r_start_inside,t_start_inside,pch=16)
+    ## source of throwing:
+    points(r_start_outside,t_start_outside,pch=16)
+    points(r_start_inside,t_start_inside,pch=16)
 
-legend(x=5,y=4,lty=1,lwd=c(rep(lthick,6),6),
-       col=c("red","orange","yellow","green","blue","purple",colours$horizon),
-       legend=c("inward throw","outward throw, bounded","critical","unbounded",
-                "interior inward","interior outward","event horizon"))
+    legend(x=5,y=4,lty=1,lwd=c(rep(lthick,6),6),
+           col=c("red","orange","yellow","green","blue","purple",colours$horizon),
+           legend=c("inward throw","outward throw, bounded","critical","unbounded",
+                    "interior inward","interior outward","event horizon"))
 }
