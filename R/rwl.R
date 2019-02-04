5d23a5604b61d3443fcea010ab3620f402a389f9
RobinHankin <hankin.robin@gmail.com>
2019-02-05 11:44:57 +1300

    rephrase

--- R/rwl.R
+++ R/rwl.R
@@ -5,7 +5,7 @@
 ## bounded outward trajectory turns into an inward trajectory after a
 ## finite time, when it starts falling towards the black hole.
 
-## This is all radial motion.
+## This file is concerned only with purely radial motion.
 
 ## In the work below, epsilon corresponds to 'energy' which is a
 ## conserved quantity in this case; epsilon=1 corresponds to an object
